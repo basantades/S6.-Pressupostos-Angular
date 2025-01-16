70122e9f2d2492da66c84d6d25bfb1e765708523
Alberto Basanta <dasantades@gmail.com>
2025-01-16 10:50:46 +0100

    iniciando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { BudgetComponent } from "./components/budget/budget.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WelcomeComponent, BudgetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/budget/budget.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-budget',
+  imports: [],
+  templateUrl: './budget.component.html',
+  styleUrl: './budget.component.scss'
+})
+export class BudgetComponent {
+budgetForm: any;
+
+}
